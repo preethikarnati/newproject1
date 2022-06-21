97ee1a892845f703a248dbfd60123eddffdcca35
preethikarnati <107621008+preethikarnati@users.noreply.github.com>
2022-06-21 08:33:17 +0530

    Revert "preethikarnati:june 20 22: Adding 40+10 removed assert"

--- src/test/java/fruitGroupid/appleArtifactid/AppTest.java
+++ src/test/java/fruitGroupid/appleArtifactid/AppTest.java
@@ -4,11 +4,10 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class AppTest 
-{
+public class AppTest {
     
- public static void main (String [] args) {
-	 
-	 int a= 40+10;
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
   }
 }
